8476485f8c2d7a770311e5b65d85561dd03c123f
Damian Kaczmarek <rush@rushbase.net>
2019-02-08 21:29:26 +0100

    Initial version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export function hello() {
-  return 'hello';
+import { decorate } from 'core-decorators';
+export { autobind as AutoBind } from 'core-decorators';
+import * as memoizee from 'memoizee';
+import { duration, unitOfTime } from 'moment';
+
+import { memoizeeRx } from 'memoizee-rx';
+
+type HumanDuration = [number, unitOfTime.DurationConstructor];
+
+export interface MemoizeOptions extends memoizee.Options {
+    observable?: boolean;
+    ttl?: HumanDuration;
+}
+
+export function Memoize(this: any, options: MemoizeOptions) {
+    const wrapperFunction = options.observable ?
+        memoizeeRx : memoizee;
+    if (options.ttl) {
+        options = {
+            ...options,
+            maxAge: duration(...options.ttl).asMilliseconds(),
+        };
+    }
+    return decorate.call(this, wrapperFunction, options);
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,24 @@
-import { hello } from '../src/index';
+import { Memoize } from '../src/index';
 
-describe('index', () => {
-  it('should greet', () => {
-    const out = hello();
+class TestClass {
+    constructor(private jestFunction: (...args: any[]) => any) {}
 
-    expect(out).toEqual('hello');
-  });
+    @Memoize({})
+    test(...args: any[]) {
+        return this.jestFunction(...args);
+    };
+};
+
+describe('Memoize', () => {
+    it('should cache subsequent values', () => {
+        const f = jest.fn((x) => x*2);
+        const instance = new TestClass(f)
+
+        const out1 = instance.test(2);
+        const out2 = instance.test(2);
+
+        expect(out1).toEqual(4);
+        expect(out2).toEqual(4);
+        expect(f).toHaveBeenCalledTimes(1);
+    });
 });
